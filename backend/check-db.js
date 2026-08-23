e4fb903b4562710a4e3574b18f07d42f87170a69
adi0537 <179014265+adi0537@users.noreply.github.com>
2026-08-24 02:05:44 +0530

    Clean up temporary diagnostic scripts

--- backend/check-db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-mongoose.connect('mongodb+srv://adityaks920:Aditya12345@cluster537.fssziok.mongodb.net/?appName=Cluster537')
-  .then(async () => {
-    const users = await mongoose.connection.db.collection('users').find({}).toArray();
-    console.log("Users:", users.map(u => ({ email: u.email, pwd: u.password })));
-    process.exit(0);
-  });
--- backend/test.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const schema = new mongoose.Schema({ name: String });
-schema.pre('save', async function(next) {
-  console.log('hook called');
-});
-const Model = mongoose.model('Test', schema);
-const doc = new Model({ name: 'test' });
-doc.save().then(() => console.log('saved')).catch(e => console.error(e));
